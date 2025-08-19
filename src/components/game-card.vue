2ae613e0c0b841db3fc6a992566fb06b0fa6f219
Ilya Alexeev <devtective.exe@gmail.com>
2025-08-19 11:20:20 -0900

    feat: add card mousemove and drag handlers

--- src/components/game-card.vue
+++ src/components/game-card.vue
@@ -6,7 +6,7 @@ import {  reactive, ref } from 'vue';
   const cardSettings = reactive({
     rotateAngle: 'rotateX(0deg) rotateY(0deg)',
     transitionStyle: 'transform 0.1s ease-out',
-    shadowAndle:'0px 10px 4px rgba(0,0,0, 0.25)',
+    shadowAndle:'0px 10px 4px rgba(0,0,0, 0.75)',
   })
 
   function cardRotateHandler(event: MouseEvent): void {
@@ -23,8 +23,8 @@ import {  reactive, ref } from 'vue';
     const rotateY = -x / 2.5;
 
     cardSettings.transitionStyle = 'transform 0.1s ease-out';
-    cardSettings.rotateAngle = `rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
-    cardSettings.shadowAndle = `${rotateY / 2}px ${-rotateX / 2}px 4px rgba(0,0,0, 0.25)`
+    cardSettings.rotateAngle = `rotateY(${rotateY}deg) rotateX(${rotateX}deg) scale(1.3)`;
+    cardSettings.shadowAndle = `${rotateY / 2}px ${-rotateX / 2}px 4px rgba(0,0,0, 0.75)`
   }
 
   function resetRotation(): void {
@@ -34,7 +34,19 @@ import {  reactive, ref } from 'vue';
   }
 
   function cardDragHandler(event: DragEvent): void {
-    console.log(event.clientX)
+    if (card.value) {
+      console.table({x: event.clientX, y: event.clientY})
+      card.value.style.position = 'absolute';
+      card.value.style.left = `${event.clientX - 140}px`;
+      card.value.style.top = `${event.clientY - 190}px`;
+    }
+  }
+
+  function dragStartHandler(event: DragEvent): void {
+    const img = new Image();
+    img.src =
+    "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='1' height='1'></svg>";
+    event.dataTransfer?.setDragImage(img, 0, 0);
   }
 </script>
 
@@ -50,43 +62,83 @@ import {  reactive, ref } from 'vue';
     draggable="true"
     @mousemove="cardRotateHandler" 
     @mouseout="resetRotation"
-    @dragstart="cardDragHandler"
+    @dragover.prevent="cardDragHandler"
+    @dragstart="dragStartHandler"
   >
+    <div class="card__content">
+      <div class="card__portrait">
+        <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExd2FjMm9ka2NqazdzY3Vvb3d0bDVqcWttemd1MThkdTc3dTUxcjJ3NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gIsyGoDfDi4TSf7SsY/giphy.gif" draggable="false"  width="200" height="200">
+      </div>
+      <div class="card__text">Lorem ipsum dolor sit amet.</div>
+    </div>
   </div>
 </template>
 
 <style scoped>
   .game-card {
-    width: 200px;
-    height: 300px;
-    background: linear-gradient( to left top, #242424, #FFFFFF);
+    aspect-ratio: 2/3;
+    padding: 15px;
+    background-image: url('../assets/card-back--dark.jpg');
+    background-size: cover;
     border-radius: 10px;
     box-shadow: var(--shadow-angle);
     transition: transform 1s ease, box-shadow 1s ease;
     transform: var(--rotate-angle);
     cursor: grab;
+    user-select: none;
   }
 
   .game-card:hover {
     transition: transform 0.01s ease, box-shadow 0.01s ease;
+    z-index: 9999;
+    border: 2px solid #928181;
   }
 
-  .eye {
+  .card__content {
+    position: relative;
+
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-content: center;
+
+    box-sizing: border-box;
     width: 100%;
-    height: 25%;
-    border-radius: 50%;
-    background-color: white;
+    height: 100%;
+    padding: 10px;
+    
+  }
+  .card__portrait {
+    position: relative;
+    top: -25px;
+
+    flex-shrink: 0;
+
+    width: 180px;
+    height: 180px;
+    border-radius: 10px;
+    overflow: hidden;
   }
 
-  .eye-ball {
-    width: 25%;
-    height: 50%;
-    background-color: black;
-    border-radius: 50%;
-    margin: 0 auto;
+  .card__portrait img {
+    position: relative;
+    top: 0;
 
+    width: 75%;
+    height: 100%;
+    
+    border-radius: 50%;
+  }
+
+  .card__text {
+    box-sizing: border-box;
+    width: 100%;
+    height: 100%;
+    padding: 10px;
+
+    border-radius: 10px;
+    background-color: rgba(255, 255, 255, 0.25);
+
+    overflow: hidden;
   }
 </style>
